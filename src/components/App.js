0c5282ed0970502a2197e2223331f2d799fb8bc3
Yufa <yufa@Yufas-iMac.local>
2022-11-01 10:01:13 -0700

    setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './style.css'
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World</h1>
+                <h1>Personal Blog</h1>
             </div>
         )
     }
